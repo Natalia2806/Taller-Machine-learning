d607877b497021b7a207945e09eba3818ff452aa
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-04-29 00:13:45 -0500

    Añadiendo los modelos a la data diabetes

--- Prima Indians Diabetes Database/Indians-Diabetes.py
+++ Prima Indians Diabetes Database/Indians-Diabetes.py
@@ -45,4 +45,95 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 x_test_out = np.array(data_test.drop(['Outcome'], axis=1))
 y_test_out = np.array(data_test.Outcome)
 
+## REGRESIÓN LOGÍSTICA
+
+logreg = LogisticRegression(solver='lbfgs', max_iter = 7600)
+
+# Entrenamiento el modelo
+logreg.fit(x_train, y_train)
+
+# MÉTRICAS
+print('*'*50)
+print('Regresión Logística')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'Accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'Accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'Accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
+
+
+## MAQUINA DE SOPORTE VECTORIAL
+
+svc = SVC(gamma='auto')
+
+# Entrenamiento el modelo
+svc.fit(x_train, y_train)
+
+# MÉTRICAS
+print('*'*50)
+print('Maquina de soporte vectorial')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
+
+
+# RANDOM FOREST
+
+random_forest = RandomForestClassifier()
+
+# Entrenar el modelo
+
+random_forest.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('Random Forest')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {random_forest.score(x_test, y_test)}')
+
+# Accuracy de Entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {random_forest.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {random_forest.score(x_test_out, y_test_out)}')
+
+
+
+## ARBOL DE DECISIÓN
+
+arbol = DecisionTreeClassifier()
+
+# Entrenamiento el modelo
+arbol.fit(x_train, y_train)
+
+# MÉTRICAS
+print('*'*50)
+print('Decisión Tree')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {arbol.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {arbol.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {arbol.score(x_test_out, y_test_out)}')
+
+
+
 
