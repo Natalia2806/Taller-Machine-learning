0a1149e3778033c67ce10fdccc502ddad8cfe6c4
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-04-28 22:58:23 -0500

    obtener y normalizar la data bank-full

---
+++ Bank Marketing/bank-full.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 21:59:47 2022
+
+@author: Naty
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+
+simplefilter(action='ignore', category=FutureWarning)
+
+#Obtener la data 
+data = pd.read_csv('bank-full.csv')
+
+#Normalizar y limpiar la data
+
+data.housing.replace(['yes', 'no'], [0, 1], inplace=True)
+
+data.marital.replace(['married', 'single', 'divorced'], [0, 1, 2], inplace=True)
+
+data.y.replace(['yes', 'no'], [0, 1], inplace=True)
+
+data.education.replace(['primary', 'secondary', 'tertiary', 'unknown'], [0, 1, 2, 3], inplace=True)
+
+data.default.replace(['no', 'yes'], [0, 1], inplace=True)
+
+data.loan.replace(['yes', 'no'], [0, 1], inplace=True)
+
+data.contact.replace(['unknown', 'cellular', 'telephone'], [0, 1, 2], inplace=True)
+
+data.poutcome.replace(['unknown', 'success', 'failure', 'other'], [0, 1, 2, 3], inplace=True)
+
+data.drop(['balance', 'duration', 'campaign', 'pdays', 'previous', 'job', 'day', 'month'], axis=1, inplace=True)
+
+age_mean = data.age.mean()
+data.age.replace(np.nan, age_mean, inplace=True)
+
+ranges = [0, 8, 15, 18, 25, 40, 60, 100]
+names = ['1', '2', '3', '4', '5', '6', '7']
+data.age = pd.cut(data.age, ranges, labels=names)
+
+data.dropna(axis=0, how='any', inplace=True)
+
+#Dividir la data en train y test
+
+
+
+
